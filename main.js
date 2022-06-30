1c327aeb056865a3d1954a6e6c4996eb5e08109c
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2022-06-30 17:44:11 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+}
+
+function preload(){}
+
+function draw(){
+
+
+
+}
+
