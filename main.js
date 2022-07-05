c9fa8de2e995d46df11fe52335dce0bad557bfd7
raseshbiyani-5501P <72201706+raseshbiyani-5501P@users.noreply.github.com>
2022-07-05 17:57:57 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -3,13 +3,52 @@ function setup() {
   canvas.center();
   video = createCapture(VIDEO);
   video.hide();
+
+  classifier = ml5.imageClassifier('MobileNet', modelLoaded);
+
 }
 
 function preload(){}
 
 function draw(){
 
-
+  image(video, 0, 0, canvas.height, canvas.width);
+  classifier.classify(video, gotResult);
 
 }
 
+function modelLoaded(){
+
+  console.log("The model has been Loaded ! :)");
+
+}
+
+pr  = "";
+
+function gotResult(error, results){
+if(error){
+
+console.error(error)
+
+}
+else{
+
+  if((results[0].label >= 0.50 )&&(pr != results[0].label)){
+
+    console.log(results);
+
+    synth  = window.speechSynthesis
+    speak_data = "The object recognised is " + results[0].label;
+    utter_this = new SpeechSynthesisUtterance(speak_data);
+    synth.speak(utter_this);
+    
+    document.getElementById("object_name").innerHTML = results[0].label;
+    per = results[0].confidence * 100;
+    document.getElementById("acc").innerHTML  = per.toFixed(2) + " %";
+
+  }
+
+}
+
+
+}
